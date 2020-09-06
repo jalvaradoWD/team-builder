2ca84deece55bf94315eb92898a5b5f075ba0746
jalvaradoWD <josealvarado1337@gmail.com>
2020-09-06 18:39:08 -0500

    Rendering team members is now working.
    - Form has been created

--- team-builder/src/App.js
+++ team-builder/src/App.js
@@ -1,7 +1,44 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 function App() {
-  return <></>;
+	const [members, setMembers] = useState([]);
+
+	useEffect(() => {
+		axios.get("https://jsonplaceholder.typicode.com/users").then((res) => {
+			const mappedMembers = res.data.map(({ id, name, email }) => {
+				return { id, name, email };
+			});
+
+			setMembers(mappedMembers);
+		});
+	});
+
+	return (
+		<>
+			<h1>Team Member Builder</h1>
+
+			<div>
+				<section>
+					{members
+						? members.map(({ name, email, id }) => {
+								return (
+									<div>
+										<h2>{name}</h2>
+										<p>{email}</p>
+										<p>{id}</p>
+									</div>
+								);
+						  })
+						: null}
+				</section>
+				<form>
+					<h2>Add Team Member</h2>
+					<input type="text" placeholder="Add team members" />
+				</form>
+			</div>
+		</>
+	);
 }
 
 export default App;
